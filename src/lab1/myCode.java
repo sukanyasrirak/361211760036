afa116fffebb9c6dca7c3b7934985ea27a3e15e7
sukanyasrirak <sukanyasrirak220740@gmail.com>
2018-06-26 11:42:10 +0700

    Hello World

---
+++ src/lab1/myCode.java
@@ -0,0 +1,21 @@
+package lab1;
+
+public class myCode {
+
+
+    public static void main(String[] args) {
+
+    System.out.println("Hello World.");
+    System.out.println("Sukanya Srirak");
+    System.out.println("361211760036");
+    System.out.println("MIT211");
+    System.out.println("RMUTSV");
+    }//main
+
+
+
+
+
+
+
+}//class
